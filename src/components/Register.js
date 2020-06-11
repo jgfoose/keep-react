83a0261ab5165e9d805101911a9f15ba7c3a2207
donutwizard666 <thebeardedwarrior@icloud.com>
2020-06-10 20:02:46 -0700

    changes

---
+++ src/components/Register.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default function SignUp(props) {
+  const [ userCreds, setUserCreds ] = useState({
+    firstname: '',
+    lastname: '',
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const [ incorrectCreds, setIncorrectCreds ] = useState({
+    isIncorrect: false
+  });
+
+  const submitHandler = e => {
+    e.preventDefault();
+
+    axios.post('')
+  }
+}
